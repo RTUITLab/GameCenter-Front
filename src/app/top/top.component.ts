f37b0969e88f626801ca4328cf6d8632b21b9d9a
Kitanin Sergei <kitanin.ser@mail.ru>
2018-08-10 04:07:08 +0300

    markup + updating table of pickedGames

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+// прототип получаемых данных
+import { IData} from '../DataInterface';
+////
+
+// сервис для операций с данными
+import {UserService} from '../user.service';
+////
+
+// уведомления
+import { ToastrService } from 'ngx-toastr';
+////
+
+// связь с другими пользователями
+import { HubService } from '../hub.service';
+////
+
 @Component({
   selector: 'app-top',
   templateUrl: './top.component.html',
@@ -7,9 +23,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _userServise: UserService, // переменная для обращения к сервису
+    private toastr: ToastrService, // уведомления
+    private _hubService: HubService, // связь с другими пользователями
+  ) { }
+
+  pickedGames: IData[]; // массив выбранных игр  типа интерфейса IData[]
+
+
+  private loadPickedGames() {// подгружаем все игры
+    this._userServise.getAllPicked().subscribe((data: IData []) => {// забираем данные из переменной в наш массив
+       this.pickedGames = data; // присваиваем данные массиву игр
+       console.log(this.pickedGames); // проверяем массив пришедших данных
+    });
+  }
 
   ngOnInit() {
+    this.loadPickedGames(); // подгружаем все выбранные игры
+    // SignalR || Связь с другими пользователями
+    this._hubService.pickNotifier.subscribe( // подписываемся на событие выбора игры,совершенного другим пользователем
+      n => this.loadPickedGames(),
+      err => console.log(err),
+      () => console.log('_hubService.pickNotifier complete')
+    );
+    this._hubService.unpickNotifier.subscribe( // подписываемся на событие удаления игры из выбранных игр,совершенного другим пользователем
+      n => this.loadPickedGames(),
+      err => console.log(err),
+      () => console.log('_hubService.unpickNotifier complete')
+    );
+    this._hubService.deleteNotifier.subscribe( // подписываемся на событие удаления игры,совершенного другим пользователем
+      n => this.loadPickedGames(),
+      err => console.log(err),
+      () => console.log('_hubService.deleteNotifier complete')
+    );
+    ////
   }
+
 
 }
